8bd4f7b093598b8f4a643091149581e521758919
ticksj <1085798032@qq.com>
2019-09-07 22:09:43 +0800

    待添加网络框架

--- app/src/main/java/com/sj/sjwanandroid/fragment/mainpage/HomePageFragment.kt
+++ app/src/main/java/com/sj/sjwanandroid/fragment/mainpage/HomePageFragment.kt
@@ -1,6 +1,14 @@
+import com.foxhis.c_network.request.CommonRequest
+import com.foxhis.c_network.request.RequestParams
 import com.sj.sjwanandroid.BaseFragment
 import com.sj.sjwanandroid.R
 
 class HomePageFragment : BaseFragment() {
     override var layoutId = R.layout.fragment_homepage;
+    val imageUrl = "https://www.baidu.com/img/bd_logo1.png"
+
+
+    override fun initData() {
+    
+    }
 }
