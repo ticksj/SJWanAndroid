b7c849da7c05e317485f29e04bd58d2dd407539c
ticksj <1085798032@qq.com>
2019-09-07 21:20:34 +0800

    基本视图框架搭建

---
+++ app/src/main/java/com/sj/sjwanandroid/fragment/mainpage/AndroidSystemFragment.kt
@@ -0,0 +1,6 @@
+import com.sj.sjwanandroid.BaseFragment
+import com.sj.sjwanandroid.R
+
+class AndroidSystemFragment : BaseFragment() {
+    override var layoutId = R.layout.fragment_androidsystem;
+}
---
+++ app/src/main/java/com/sj/sjwanandroid/fragment/mainpage/ProjectFragment.kt
@@ -0,0 +1,6 @@
+import com.sj.sjwanandroid.BaseFragment
+import com.sj.sjwanandroid.R
+
+class ProjectFragment : BaseFragment() {
+    override var layoutId = R.layout.fragment_project;
+}
